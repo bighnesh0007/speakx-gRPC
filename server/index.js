199216a549cca187f390c1f1bdaeed45ae8f7a70
bighnesh0007 <112768825+bighnesh0007@users.noreply.github.com>
2025-01-26 01:51:23 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ const packageDefinition = protoLoader.loadSync(PROTO_PATH, {
 
 const questSearchProto = grpc.loadPackageDefinition(packageDefinition).questsearch;
 
-const uri = process.env.MONGO_URI || "mongodb://localhost:27017";
+const uri = process.env.MONGO_URI || "mongodb+srv://shashankkrpandey123:8QDOACIcrdLb8l2G@cluster0.qbhmqbs.mongodb.net/speakx?retryWrites=true&w=majority";
 const client = new MongoClient(uri);
 
 async function searchQuestionsByTitle(call, callback) {
